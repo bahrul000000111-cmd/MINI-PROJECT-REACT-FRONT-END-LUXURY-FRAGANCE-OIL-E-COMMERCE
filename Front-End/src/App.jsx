08875286d31e831ac2fee606da896c3c7389fa48
bahrul000000111-cmd <bahrul000000111@gmail.com>
2026-04-25 17:54:50 +0800

    feat : menambahkan route untuk Footer

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -6,6 +6,7 @@ import SocialProof from './components/SocialProof'
 import StatsSection from './components/StatsSection';
 import ShopByHotel from './components/ShopByHotel';
 import ComparisonSection from './components/ComparisonSection';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <ComparisonSection />
       <StatsSection/>
       <ShopByHotel/>
+      <Footer/>
     </>
   )
 }
